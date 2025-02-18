c9fcfbcd34fa222f6515a4299c190284f1959de4
Codewithimad <imadhussain6655@gmail.com>
2025-02-18 13:59:16 +0500

    Changes in all

--- middlewares/multer-middleware.js
+++ middlewares/multer-middleware.js
@@ -8,6 +8,7 @@ const tempDir = path.join(__dirname, './upload/temp');
 // Ensure the temp directory exists
 if (!fs.existsSync(tempDir)) {
     fs.mkdirSync(tempDir, { recursive: true }); // Creates the directory if missing
+    console.log(`Created temp directory: ${tempDir}`);
 }
 
 // File type validation
